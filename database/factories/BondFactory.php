9eaca644f12799af2a3a22c32cbd55cc25101c5c
Ilgar <eliyev.iq@gmail.com>
2022-12-05 18:15:55 +0400

    Factory last_circulation_date period increased

--- database/factories/BondFactory.php
+++ database/factories/BondFactory.php
@@ -21,7 +21,7 @@ class BondFactory extends Factory
         $period_of_calculation_of_percentages = [360,364,365];
         return [
             'date_of_issue'                        => Carbon::now()->addDays(rand(1,10)),
-            'last_circulation_date'                => Carbon::now()->addMonths(rand(3,10)),
+            'last_circulation_date'                => Carbon::now()->addMonths(rand(15,36)),
             'price'                                => rand(5,99),
             'frequency_of_payment'                 => $frequency_of_payment[array_rand($frequency_of_payment)],
             'period_of_calculation_of_percentages' => $period_of_calculation_of_percentages[array_rand($period_of_calculation_of_percentages)],
