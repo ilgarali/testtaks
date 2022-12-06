56e79a4819e51d628ae0a18bd23d5d7f93e33a1a
Ilgar <eliyev.iq@gmail.com>
2022-12-06 11:53:46 +0400

    $fillable columns added, bond relationship added to Order

--- app/Models/Bond.php
+++ app/Models/Bond.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Bond extends Model
 {
     use HasFactory;
+    protected $fillable = ['date_of_issue', 'last_circulation_date', 'price', 'frequency_of_payment',
+        'period_of_calculation_of_percentages', 'coupon_rate'];
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = ['bond_id', 'order_date', 'number_of_bonds_bought'];
+
+    public function bond(): BelongsTo
+    {
+        return $this->belongsTo(Bond::class);
+    }
+
 }
