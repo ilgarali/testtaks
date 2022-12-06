8b2131c03b18cd9478af98ef0bd5b30d7ab5dfde
Ilgar <eliyev.iq@gmail.com>
2022-12-06 11:55:09 +0400

    Calculation extracted to CalculateDates

--- app/Helpers/CalculateDates.php
+++ app/Helpers/CalculateDates.php
@@ -2,10 +2,60 @@
 
 namespace App\Helpers;
 
+use Carbon\Carbon;
+
 class CalculateDates
 {
-    public function calculateDates(bool $bayDay = true)
-    {
+    private float $periodDurationByDay = 0;
+    private float $periodDurationByMonth = 0;
+    private array $dates = [];
 
+    public function calculate(
+        int $period_of_calculation_of_percentages, int $frequency_of_payment,
+        string $date_of_issue, string $last_circulation_date): array
+    {
+        switch ($period_of_calculation_of_percentages){
+            case 360:
+                $this->periodDurationByDay = 12 / $frequency_of_payment * 30;
+                break;
+            case 364:
+                $this->periodDurationByDay = 364 / $frequency_of_payment;
+                break;
+            case 365:
+                $this->periodDurationByMonth = 12 / $frequency_of_payment;
+                break;
+        }
+
+        if ($this->periodDurationByDay != 0) {
+            $nextPaymentDate = Carbon::parse($date_of_issue)->addDays($this->periodDurationByDay);
+            $this->calculateByDate($nextPaymentDate, $last_circulation_date, true);
+        }
+        if ($this->periodDurationByMonth != 0) {
+            $nextPaymentDate = Carbon::parse($date_of_issue)->addMonths($this->periodDurationByMonth);
+
+            $this->calculateByDate($nextPaymentDate, $last_circulation_date, false);
+        }
+
+        return $this->dates;
     }
+
+    public function calculateByDate($nextPaymentDate, $last_circulation_date, $day = true)
+    {
+        $i = 0;
+        while ($nextPaymentDate < $last_circulation_date){
+            if ($nextPaymentDate->format('D') == 'Sat') {
+                $this->dates[$i]['date'] = $nextPaymentDate->addDays(2)->format('Y-m-d');
+            }elseif ($nextPaymentDate->format('D') == 'Mon'){
+                $this->dates[$i]['date'] = $nextPaymentDate->addDay()->format('Y-m-d');
+            }else {
+                $this->dates[$i]['date'] = $nextPaymentDate->format('Y-m-d');
+            }
+            $i++;
+            $nextPaymentDate = $day ?
+                Carbon::parse($nextPaymentDate)->addDays($this->periodDurationByDay)
+                :
+                Carbon::parse($nextPaymentDate)->addMonths($this->periodDurationByMonth);
+        }
+    }
+
 }
--- app/Http/Resources/BondResource.php
+++ app/Http/Resources/BondResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\CalculateDates;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -9,61 +10,14 @@ class BondResource extends JsonResource
 {
     public static $wrap = null;
 
-    private float $periodDurationByDay = 0;
-    private float $periodDurationByMonth = 0;
-    private array $dates = [];
     public function toArray($request)
     {
-        $this->calculate();
+        $calculateDate = new CalculateDates();
         return [
-            'dates' => $this->dates
+            'dates' => $calculateDate->calculate($this->period_of_calculation_of_percentages,$this->frequency_of_payment,
+            $this->date_of_issue, $this->last_circulation_date
+            ),
         ];
-    }
-
-    public function calculate()
-    {
-        switch ($this->period_of_calculation_of_percentages){
-            case 360:
-                $this->periodDurationByDay = 12 / $this->frequency_of_payment * 30;
-                break;
-            case 364:
-                $this->periodDurationByDay = 364 / $this->frequency_of_payment;
-                break;
-            case 365:
-                $this->periodDurationByMonth = 12 / $this->frequency_of_payment;
-                break;
-        }
-
-        if ($this->periodDurationByDay != 0) {
-            $nextPaymentDate = Carbon::parse($this->date_of_issue)->addDays($this->periodDurationByDay);
-            $this->calculateByDate($nextPaymentDate, true);
-        }
-        if ($this->periodDurationByMonth != 0) {
-            $nextPaymentDate = Carbon::parse($this->date_of_issue)->addMonths($this->periodDurationByMonth);
-
-            $this->calculateByDate($nextPaymentDate, false);
-        }
-
-
-    }
-
-    public function calculateByDate($nextPaymentDate, $day = true)
-    {
-        $i = 0;
-        while ($nextPaymentDate < $this->last_circulation_date){
-            if ($nextPaymentDate->format('D') == 'Sat') {
-                $this->dates[$i]['date'] = $nextPaymentDate->addDays(2)->format('Y-m-d');
-            }elseif ($nextPaymentDate->format('D') == 'Mon'){
-                $this->dates[$i]['date'] = $nextPaymentDate->addDay()->format('Y-m-d');
-            }else {
-                $this->dates[$i]['date'] = $nextPaymentDate->format('Y-m-d');
-            }
-            $i++;
-            $nextPaymentDate = $day ?
-                Carbon::parse($nextPaymentDate)->addDays($this->periodDurationByDay)
-                :
-                Carbon::parse($nextPaymentDate)->addMonths($this->periodDurationByMonth);
-        }
     }
 
 }
